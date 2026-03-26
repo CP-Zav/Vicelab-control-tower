f5120b9778ee0bca8547dd52a6e15b119ac6e3d7
CP-Zav <zavell.mercer@proton.me>
2026-03-26 15:24:47 +1000

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,19 @@
+export const metadata = {
+  title: "ViceLab Control Tower",
+  description: "Command center dashboard"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body style={{
+        margin: 0,
+        background: "#0a0a0a",
+        color: "#fff",
+        fontFamily: "system-ui"
+      }}>
+        {children}
+      </body>
+    </html>
+  );
+}
